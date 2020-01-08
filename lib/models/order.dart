7a5a5c63e1546974d00c15a0feac001ff669088c
YUSUF HERI <yusufheri64@gmail.com>
2020-01-09 00:12:03 +0100

    Data and Models

---
+++ lib/models/order.dart
@@ -0,0 +1,16 @@
+import 'package:food_delivery/models/food.dart';
+import 'package:food_delivery/models/restaurant.dart';
+
+class Order {
+  final Restaurant restaurant;
+  final Food food;
+  final int quantity;
+  final String date;
+
+  Order({
+    this.restaurant,
+    this.food,
+    this.date,
+    this.quantity
+  });
+}
